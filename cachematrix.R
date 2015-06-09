5ad15a2027fba4c1cbf098ff2d7dce48eecfb7e5
fahdill <fahdill@users.noreply.github.com>
2015-06-09 14:37:05 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+# ASSIGNMENT 2, NOT FINISHED
+# caching matrix
 
-## Write a short comment describing this function
+# makes an object that caches (eventually) a matrix and its inverse
+# packs itself with methods that enable it to do so
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# initiate object with empty matrix, if no arguments are given
+makeCacheMatrix = function(x = matrix()) {
+    
+    # empty placeholder for inverse matrix
+    matInverse = NULL
+    
+    # set new matrix from outside
+    set = function(x2) {
+        
+        # "<<-" enables the scope to be extended from outside to inside the function
+        # example: 
+        # let  m = makeMatrix()  then  m$setMatrix(some_matrix) sets new matrix to m
+        x <<- x2
+        
+        # clear inverted matrix when assigning a new matrix to object
+        matInverse <<- NULL
+        message ("New matrix made, cache cleared.")
+    }
+    
+    # get matrix that is set
+    get = function() {x}
+    
+    # make a way to set inverted matrix from outside
+    # basically, set matInverded to whatever you pass 
+    # to this function called from outside
+    # basically: 
+    # let m = makeMatrix(some_matrix)
+    # its then possible to:
+    # m$setInverse(solve(m$getMatrix()))  to set the inverse inside object
+    setInv = function(inv) {matInverse <<- inv}
+    
+    # a way to get inverted matrix now existing in this object
+    getInv = function() {matInverse}
+    
+    # return a list of properties/methods for this object
+    list(
+        setMatrix = set, 
+        getMatrix = get,
+        setInverse = setInv,
+        getInverse = getInv
+    )
 }
 
 
+
+## TO DO
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
